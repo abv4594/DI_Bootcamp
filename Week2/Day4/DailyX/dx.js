2ec18705a7803e2bd69449e28195c1a1fa8dfd9d
abv4594 <abv4594@gmail.com>
2021-10-13 21:28:41 +0300

    Done daily x WK2D4

---
+++ Week2/Day4/DailyX/dx.js
@@ -0,0 +1,33 @@
+// Instructions
+// Prompt the user for several words(separated by commas).
+// Put the words into an array.
+//     Console.log the words one per line, in a rectangular frame as seen below.
+// Check out the Hints and Requirements below.
+// For example, if the user gives you:
+// Hello, World, in, a, frame
+// you will transform it to: ["Hello", "World", "in", "a", "frame"]
+// that will get displayed as:
+
+// stars and words
+
+function maxLength(arr) {
+    return Math.max(...arr.map((word)=>{return word.length}))
+}
+
+function fillSpace(str,length) {//returns a new str with spaces between str.length till length
+    return str+(' '.repeat(length-str.length))
+}
+
+function getWords() { //returns array with user input 
+    return prompt("Please enter words (separated by commas)").split(",");
+}
+
+// main
+words = getWords();
+maxl = maxLength(words);
+
+console.log('*'.repeat(maxl+4)) //top outline
+for (word of words){
+    console.log('* '+fillSpace(word,maxl)+' *')
+}
+console.log('*'.repeat(maxl+4)) //bottom outline
