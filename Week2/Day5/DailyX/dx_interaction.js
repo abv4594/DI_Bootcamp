2c3df6ab46e7210df5ef8a02ba0b5bc7017642f9
abv4594 <abv4594@gmail.com>
2021-10-17 10:06:58 +0300

    Fixed beers

--- Week2/Day5/DailyX/dx_interaction.js
+++ Week2/Day5/DailyX/dx_interaction.js
@@ -1,14 +1,16 @@
-let num = 10;
+let num = parseInt(prompt("How many bottles to start?"));
 let pace = 1;
 do { 
     console.log(`${num} ${num == 1 ? "bottle" : "bottles"} of beer on the wall`);
     console.log(`${num} ${num == 1 ? "bottle" : "bottles"} of beer on the wall`);
-    console.log(`${num} bottles of beer`);
+    console.log(`${num} ${num == 1 ? "bottle" : "bottles"} of beer`);
     if (num >= pace) {
-        console.log(`Take ${pace} down, pass them around`)
+        console.log(`Take ${pace} down, pass ${pace == 1 ? "it " : "them "} around`)
     } else { console.log(`Oops not enough beer to take ${pace} bottles down`) }
 
     num=num-pace;
     pace=pace+1;
 
-} while((num - pace) >= 0)
+} while(num>=0)
+
+
